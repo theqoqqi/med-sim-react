a9729f1707d9e606bf7d4dbcdfdd5cdf5b7e2a77
Qoqqi <sazykin.georgii@gmail.com>
2023-07-09 18:24:50 +0300

    Рефакторинг: из DiseaseFactory вынесен родительский класс BaseEffectorFactory

---
+++ src/simulation/effectors/BaseEffectorFactory.js
@@ -0,0 +1,71 @@
+import Disease from './Disease.js';
+import NumberParameter from '../parameters/NumberParameter.js';
+import CompositeParameter from '../parameters/CompositeParameter.js';
+import ParameterEffect from '../parameters/ParameterEffect.js';
+import Easing from '../utils/Easing.js';
+
+export class BaseEffectorFactory {
+
+    allDescriptors;
+
+    constructor(effectorDescriptors) {
+        this.allDescriptors = effectorDescriptors;
+    }
+
+    /**
+     * @return Effector
+     * */
+    createInstance(descriptor, human, effects) {
+        throw new Error('Unsupported operation. This method must be overwritten in subclass.');
+    }
+
+    createByIndex(human, index) {
+        const descriptor = this.allDescriptors[index];
+
+        if (!descriptor) {
+            throw new Error(`Descriptor with index ${index} not found.`);
+        }
+
+        return this.createFromDescriptor(human, descriptor);
+    }
+
+    createFromDescriptor(human, descriptor) {
+        const effects = Object.entries(descriptor.effects)
+            .flatMap(([parameterName, effectDescriptor]) => {
+                return this.createParameterEffects(human, parameterName, effectDescriptor);
+            });
+
+        return this.createInstance(descriptor, human, effects);
+    }
+
+    createParameterEffects(human, parameterPath, descriptor) {
+        let parameter = human.getParameter(parameterPath);
+
+        if (parameter instanceof NumberParameter) {
+            return [
+                this.createNumberParameterEffect(descriptor, parameterPath)
+            ];
+        }
+
+        if (parameter instanceof CompositeParameter) {
+            return Object.entries(descriptor)
+                .flatMap(([parameterName, effectDescriptor]) => {
+                    let combinedPath = parameterPath + '.' + parameterName;
+
+                    return this.createParameterEffects(human, combinedPath, effectDescriptor);
+                });
+        }
+
+        throw new Error('Invalid parameter: ' + parameterPath);
+    }
+
+    createNumberParameterEffect(descriptor, parameterName) {
+        if (typeof descriptor === 'number') {
+            return new ParameterEffect(parameterName, descriptor, Easing.linear);
+        }
+
+        let easingFunction = Easing.getFunction(descriptor.easingFunction, descriptor.easingFunctionOptions);
+
+        return new ParameterEffect(parameterName, descriptor.impact, easingFunction);
+    }
+}
--- src/simulation/effectors/DiseaseFactory.js
+++ src/simulation/effectors/DiseaseFactory.js
@@ -1,81 +1,26 @@
-import ParameterEffect from '../parameters/ParameterEffect';
-import Disease from './Disease';
-import Easing from '../utils/Easing';
-import NumberParameter from '../parameters/NumberParameter';
-import CompositeParameter from '../parameters/CompositeParameter';
 import Random from '../utils/Random';
+import {BaseEffectorFactory} from './BaseEffectorFactory.js';
+import Disease from './Disease.js';
 
-export default class DiseaseFactory {
+export default class DiseaseFactory extends BaseEffectorFactory {
 
-    #allDiseaseDescriptors;
-
-    constructor(diseaseDescriptors) {
-        this.#allDiseaseDescriptors = diseaseDescriptors;
-    }
-
-    createByIndex(human, index) {
-        const descriptor = this.#allDiseaseDescriptors[index];
-
-        if (!descriptor) {
-            throw new Error(`Disease with index ${index} not found.`);
-        }
-
-        return this.#createFromDescriptor(human, descriptor);
+    createInstance(descriptor, human, effects) {
+        return new Disease(descriptor, human, effects);
     }
 
     createRandom(human) {
-        const randomDescriptor = Random.weightedByField(this.#allDiseaseDescriptors, 'chancePerDay');
+        const randomDescriptor = Random.weightedByField(this.allDescriptors, 'chancePerDay');
 
-        return this.#createFromDescriptor(human, randomDescriptor);
+        return this.createFromDescriptor(human, randomDescriptor);
     }
 
     createRandomSet(human) {
-        return this.#allDiseaseDescriptors
+        return this.allDescriptors
             .filter(descriptor => {
                 return Math.random() < descriptor.chancePerDay;
             })
             .map(descriptor => {
-                return this.#createFromDescriptor(human, descriptor);
+                return this.createFromDescriptor(human, descriptor);
             });
-    }
-
-    #createFromDescriptor(human, descriptor) {
-        const effects = Object.entries(descriptor.effects)
-            .flatMap(([parameterName, effectDescriptor]) => {
-                return this.createParameterEffects(human, parameterName, effectDescriptor);
-            });
-
-        return new Disease(descriptor, human, effects);
-    }
-
-    createParameterEffects(human, parameterPath, descriptor) {
-        let parameter = human.getParameter(parameterPath);
-
-        if (parameter instanceof NumberParameter) {
-            return [
-                this.createNumberParameterEffect(descriptor, parameterPath)
-            ];
-        }
-
-        if (parameter instanceof CompositeParameter) {
-            return Object.entries(descriptor)
-                .flatMap(([parameterName, effectDescriptor]) => {
-                    let combinedPath = parameterPath + '.' + parameterName;
-
-                    return this.createParameterEffects(human, combinedPath, effectDescriptor);
-                });
-        }
-
-        throw new Error('Invalid parameter: ' + parameterPath);
-    }
-
-    createNumberParameterEffect(descriptor, parameterName) {
-        if (typeof descriptor === 'number') {
-            return new ParameterEffect(parameterName, descriptor, Easing.linear);
-        }
-
-        let easingFunction = Easing.getFunction(descriptor.easingFunction, descriptor.easingFunctionOptions);
-
-        return new ParameterEffect(parameterName, descriptor.impact, easingFunction);
     }
 }
