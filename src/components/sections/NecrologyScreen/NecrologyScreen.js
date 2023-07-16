214a96d458b24e4ad542fd8de964138e5717e081
Qoqqi <sazykin.georgii@gmail.com>
2023-07-16 14:39:25 +0300

    Добавлено отображение назначенных препаратов у погибших

--- src/components/sections/NecrologyScreen/NecrologyScreen.js
+++ src/components/sections/NecrologyScreen/NecrologyScreen.js
@@ -9,6 +9,8 @@ import HumanList from '../../organisms/HumanList/HumanList.js';
 import Conditional from '../../atoms/Conditional/Conditional.js';
 import Center from '../../atoms/Center/Center.js';
 import ParametersOverview from '../../organisms/ParametersOverview/ParametersOverview.js';
+import classNames from 'classnames';
+import TreatmentCoursesOverview from '../../organisms/TreatmentCoursesOverview/TreatmentCoursesOverview.js';
 
 NecrologyScreen.propTypes = {
     simulation: PropTypes.instanceOf(Simulation),
@@ -53,6 +55,15 @@ function NecrologyScreen({ simulation }) {
                         </span>
                     </SectionHeader>
                     <SectionBody scrollable>
+                        <div className={classNames(styles.treatmentCoursesOverview, 'px-2 py-2')}>
+                            <div className='px-2'>
+                                <h6>Назначенные медикаменты</h6>
+                            </div>
+                            <TreatmentCoursesOverview human={selectedHuman} />
+                        </div>
+                        <div className='px-3 pt-2'>
+                            <h6>Результаты анализов</h6>
+                        </div>
                         <ParametersOverview human={selectedHuman} />
                     </SectionBody>
                 </Conditional>
